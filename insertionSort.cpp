7ef8826139ea3ba0af9ebc161e8881b054365808
26nafis <spalanzaninafis@gmail.com>
2025-03-25 00:09:11 +0700

    membuat n tidak lebih dari 20 dan membuat variable inputan n

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -2,3 +2,19 @@
 using namespace std;
 
 int arr [20]; // membuat array dengan panjang 20 
+int n; // membuat variable inputan n
+
+void input ()
+{//procedure input
+while (true)
+{
+cout << "Memasukan Jumlah Data Pada Array : "; //membuat inputan jumlah element array
+cin >> n; // memanggil variable inputan n
+
+if (n <= 20)
+{//membuat kondisi n tidak lebih dari 20
+break;
+}
+else
+}
+}
