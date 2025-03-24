7a1ec8c3b6d116f894e5f29cddd678cfdc4d7e9a
26nafis <spalanzaninafis@gmail.com>
2025-03-25 00:18:23 +0700

    membuat tampilan data elemen array dan memnggunalan perulangan for

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -20,4 +20,12 @@ else
 cout << "\nArray yang anda masukan maksimal 20 elemen.\n"; //menampilkan pesan jika data lebih dari 20
 }
 }
+
+cout << endl; // membuat jarak per baris program
+cout << "======================" << endl; //membuat tampilan susunan data elemen array
+cout << "Masukan Element Array" << endl;
+cout << "=====================" << endl;
+
+for (int i = 0; i < n; i++) // menggunakan perulangan for untuk menyimpan data pada array
+
 }
