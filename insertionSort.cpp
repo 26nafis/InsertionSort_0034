b0e489c282c0eacb91f38893702dfa2a63a17987
26nafis <spalanzaninafis@gmail.com>
2025-03-25 00:21:15 +0700

    memasukan nilai data n

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -20,12 +20,13 @@ else
 cout << "\nArray yang anda masukan maksimal 20 elemen.\n"; //menampilkan pesan jika data lebih dari 20
 }
 }
-
 cout << endl; // membuat jarak per baris program
 cout << "======================" << endl; //membuat tampilan susunan data elemen array
 cout << "Masukan Element Array" << endl;
 cout << "=====================" << endl;
 
 for (int i = 0; i < n; i++) // menggunakan perulangan for untuk menyimpan data pada array
-
+{
+cout << "Data ke-" << (i + 1) << ": "; //Memasukan atau mengimputkan nilai data n
+}
 }
